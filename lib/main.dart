cfbc3490a6d7ea67f209c20f7e9340c2efc0295e
Moises Yepez <Moisesyepez@email.arizona.edu>
2022-06-11 17:49:48 -0700

    next

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:syncfusion_flutter_charts/charts.dart";
+//import "package:syncfusion_flutter_charts/charts.dart";
 import 'package:flutter/physics.dart';
 
 void main() => runApp(MyApp());
@@ -115,7 +115,7 @@ class _HomeState extends State<Home> {
             onPressed: (){
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => _ChartApp()),
+                MaterialPageRoute(builder: (context) => Pie()),
               );
             }
           ),
@@ -142,71 +142,14 @@ class _HomeState extends State<Home> {
 
 
 
-
-
-class _ChartApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: _MyHomePage(),
-    );
-  }
-}
-
-class _MyHomePage extends StatefulWidget {
-  // ignore: prefer_const_constructors_in_immutables
-  _MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<_MyHomePage> {
-  late List<_ChartData> data;
-  late TooltipBehavior _tooltip;
-
-  @override
-  void initState() {
-    data = [
-      _ChartData('CHN', 12),
-      _ChartData('GER', 15),
-      _ChartData('RUS', 30),
-      _ChartData('BRZ', 6.4),
-      _ChartData('IND', 14)
-    ];
-    _tooltip = TooltipBehavior(enable: true);
-    super.initState();
-  }
-
+class Pie extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Syncfusion Flutter chart'),
-        ),
-        body: SfCartesianChart(
-            primaryXAxis: CategoryAxis(),
-            primaryYAxis: NumericAxis(minimum: 0, maximum: 40, interval: 10),
-            tooltipBehavior: _tooltip,
-            series: <ChartSeries<_ChartData, String>>[
-              DoughnutSeries<_ChartData, String>(
-                  dataSource: data,
-                  xValueMapper: (_ChartData data, _) => data.x,
-                  yValueMapper: (_ChartData data, _) => data.y,
-                  name: 'Gold',
-                  color: Color.fromRGBO(8, 142, 255, 1))
-            ]));
+    appBar: AppBar(
+        title: const Text('KMS'),
+    ),
+    );
   }
 }
-
-class _ChartData {
-  _ChartData(this.x, this.y);
-  final String x;
-  final double y;
-}
-
-
-
-
 
